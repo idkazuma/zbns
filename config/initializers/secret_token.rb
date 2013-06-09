b94f11215a27de4b4107402ba88ba2ea822d3ee0
idkazuma <idkazuma@gmail.com>
2013-06-09 17:44:21 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zbns::Application.config.secret_token = '79790f2ad92abbc48ddb6158b16ef950c5acba970852a720d0d8ac1cade167490aeedd0efbc69fd29f0f1d55521d9b6c305e537c1c2f1206e924b7a89ecb157d'
